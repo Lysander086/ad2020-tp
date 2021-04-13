f37a4cabadb526b357bea50e62d96b09c36ccd3a
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-04-13 23:44:05 +0800

    bm

--- chapter-14/ad-sponsor/src/test/java/com/imooc/ad/Application.java
+++ chapter-14/ad-sponsor/src/test/java/com/imooc/ad/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
+ * markz
  */
 @SpringBootApplication
 public class Application {
